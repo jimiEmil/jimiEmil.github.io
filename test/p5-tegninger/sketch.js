017abb8e35ff07309f6ad949b8424b6a819a8db3
JimiZakariassen <zakariassenjimi@gmail.com>
2019-11-14 13:23:13 +0100

    addede p5 filer

---
+++ test/p5-tegninger/sketch.js
@@ -0,0 +1,81 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  
+  background(0, 200, 500);
+  
+  //krop
+  line(200, 350, 200, 380)
+  //højre ben
+  line(200, 380, 210, 400)
+  //venstre ben
+  line(200, 380, 190, 400)
+  //venstre arm
+  line(200, 360, 210, 370)
+  //højre arm
+  line(200, 360, 190, 370)
+  //hoved
+  circle(200, 350, 20)
+  
+  
+  
+  //krop
+  line(150, 350, 150, 380)
+  //højre ben
+  line(150, 380, 160, 400)
+  //venstre ben
+  line(150, 380, 140, 400)
+  //venstre arm
+  line(150, 360, 160, 370)
+  //højre arm
+  line(150, 360, 140, 370)
+  //hoved
+  circle(150, 350, 20)
+  
+   //krop
+  line(100, 350, 100, 380)
+  //højre ben
+  line(100, 380, 110, 400)
+  //venstre ben
+  line(100, 380, 90, 400)
+  //venstre arm
+  line(100, 360, 110, 370)
+  //højre arm
+  line(100, 360, 90, 370)
+  //hoved
+  circle(100, 350, 20)
+
+  
+     //krop
+  line(340, 350, 340, 320)
+  //højre ben
+  line(360, 400, 340, 350)
+  //venstre ben
+  line(320, 400, 340, 350)
+  //venstre arm
+  line(320, 320, 340, 340)
+  //højre arm
+  line(360, 320, 340, 340)
+  //hoved
+  circle(340, 320, 20)
+  
+  triangle(150, 250, 300, 300, 200, 100)
+  
+  //talepult
+  circle(180, 150, 300)
+ 
+  
+  rect(300, 340, 20, 60)
+  //stage
+  rect(300, 370, 100, 50)
+  
+  
+  rect(165, 80, 10, -40)
+  rect(100, 70, 150, 150)
+  rect(125, 180, 100, 20)
+  rect(110, 100, 50, 40)
+  rect(190, 100, 50, 40)
+  circle(170, 40, 20)
+}
